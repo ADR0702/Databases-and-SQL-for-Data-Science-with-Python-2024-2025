4895e2a125cadce06d62d3c874034e5cfe6a50bb
ADR0207 <catalinadriandumitru@outlook.com>
2025-03-14 17:07:06 +0200

    Instructiuni SQL

---
+++ instructiuni.sql
@@ -0,0 +1,34 @@
+SHOW DATABASES;
+
+CREATE DATABASE IF NOT EXISTS baza_de_date_4;
+
+SHOW DATABASES;
+
+USE baza_de_date_4;
+
+CREATE TABLE IF NOT EXISTS iepuri(nume VARCHAR(255), varsta INT);
+
+SELECT * FROM iepuri;
+
+INSERT INTO iepuri VALUES("BUGS", 5);
+
+SELECT * FROM iepuri;
+
+INSERT INTO iepuri SET varsta= 9, nume="Morcoveata";
+
+SELECT * FROM iepuri WHERE varsta=5;
+
+SELECT * FROM iepuri WHERE nume= "Bugs";
+
+
+INSERT INTO iepuri VALUES("BUGS", 7);
+INSERT INTO iepuri VALUES("BUGS", 10);
+INSERT INTO iepuri VALUES("BUGS", 12);
+
+SELECT * FROM iepuri;
+SET sql_safe_updates=0;
+UPDATE iepuri SET nume= "Bugs Bunny" WHERE nume= "Bugs";
+SELECT * FROM iepuri;
+
+SET sql_safe_updates=1;
+
